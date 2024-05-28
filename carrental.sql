67e34e01bd92979fe8785d1450083837cccee359
M4gnez <118939186+M4gnez-thxforstealingmyusername@users.noreply.github.com>
2024-05-28 19:26:57 +0200

    Date format
    changed dates to string

--- carrental.sql
+++ carrental.sql
@@ -1,6 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+01:00";
+SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -10,67 +10,49 @@ SET time_zone = "+01:00";
 CREATE DATABASE IF NOT EXISTS `carrental` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `carrental`;
 
-CREATE TABLE `car` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `car` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `producer` varchar(20) NOT NULL,
   `make` varchar(40) NOT NULL,
   `year` int(11) NOT NULL,
   `milage` int(11) NOT NULL,
   `fuel` enum('diesel','gas','electric','hybrid') NOT NULL,
   `price` double NOT NULL,
-  `imageUrl` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `imageUrl` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `contact` (
-  `id` int(11) NOT NULL,
+INSERT INTO `car` (`id`, `producer`, `make`, `year`, `milage`, `fuel`, `price`, `imageUrl`) VALUES
+(1, 'Volkswagen', 'passat b7', 2008, 80000, 'diesel', 130, 'https://www.signeda.lt/image/catalog/BLOGO%20NUOTRAUKOS/2021-06-07%20Volkswagen%20Passat%20B7%20USA%20type/passat-3.png'),
+(2, 'BMW', 'S7', 2011, 110000, 'diesel', 200, 'https://lodz.premiumarena.pl/assets/Uploads/_resampled/ResizedImage1078719-BMW-serii-7-2023.png');
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `lastName` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `phone` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `phone` varchar(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `rent` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `rent` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `carId` int(11) NOT NULL,
-  `startDate` datetime NOT NULL,
-  `endDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `startDate` varchar(60) NOT NULL,
+  `endDate` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `lastName` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(60) NOT NULL,
-  `phone` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-ALTER TABLE `car`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `rent`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `car`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `rent`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  `phone` varchar(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
